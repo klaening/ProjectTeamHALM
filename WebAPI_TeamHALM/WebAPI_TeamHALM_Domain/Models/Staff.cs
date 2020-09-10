f0582fda8f32347e0e2bbebebbcb09326c804847
Mikael <m.klaening@gmail.com>
2020-09-10 11:13:10 +0200

    WepAPI working with local PragueParking Database

---
+++ WebAPI_TeamHALM/WebAPI_TeamHALM_Domain/Models/Staff.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebAPI_TeamHALM_Domain
+{
+    public class Staff
+    {
+        public string Name { get; set; }
+        public int DepartmentsID { get; set; }
+    }
+}
---
+++ WebAPI_TeamHALM/WebAPI_TeamHALM_Domain/Services/Staff/StaffService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPI_TeamHALM_Domain
+{
+    public class StaffService : IStaffService
+    {
+        private readonly IStaffRepository _staffRepository;
+
+        public StaffService(IStaffRepository staffRepository)
+        {
+            _staffRepository = staffRepository;
+        }
+
+        public async Task<IEnumerable<Staff>> GetStaff()
+        {
+            return await _staffRepository.GetStaff();
+        }
+
+        public async Task<Staff> GetStaff(int id)
+        {
+            return await _staffRepository.GetStaff(id);
+        }
+    }
+}
