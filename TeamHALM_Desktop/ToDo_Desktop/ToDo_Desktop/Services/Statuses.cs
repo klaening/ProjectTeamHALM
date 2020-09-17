ed16b5c2fd044588239c136a5e94af961bb5502d
Mikael <m.klaening@gmail.com>
2020-09-17 18:34:54 +0200

    Statuses Enum

---
+++ TeamHALM_Desktop/ToDo_Desktop/ToDo_Desktop/Services/Statuses.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDo_Desktop.Services
+{
+    public enum Statuses
+    {
+        Pending = 1,
+        Accepted,
+        Denied,
+        Review,
+        Completed
+    }
+}
