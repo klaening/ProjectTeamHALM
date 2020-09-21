9d80e979b75f2881b88bc29ba42dc10c25ca8e7f
Mikael <m.klaening@gmail.com>
2020-09-21 11:03:35 +0200

    StringToDecimalConverter to bind a textbox to Property

---
+++ TeamHALM_Desktop/ToDo_Desktop/ToDo_Desktop/Converters/StringToDecimalConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace ToDo_Desktop.Converters
+{
+    public class StringToDecimalConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            return value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            decimal number;
+
+            decimal.TryParse(value.ToString(), out number);
+            return number;
+        }
+    }
+}
