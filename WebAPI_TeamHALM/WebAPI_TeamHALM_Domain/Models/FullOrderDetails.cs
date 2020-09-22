ac5d9c3f5a8c7eda8dc08047e81fc681fc21d621
Mikael <m.klaening@gmail.com>
2020-09-22 11:11:39 +0200

    OrderTitle in FullOrderDetails Model

--- WebAPI_TeamHALM/WebAPI_TeamHALM_Domain/Models/FullOrderDetails.cs
+++ WebAPI_TeamHALM/WebAPI_TeamHALM_Domain/Models/FullOrderDetails.cs
@@ -7,6 +7,7 @@ namespace WebAPI_TeamHALM_Domain.Models
     public class FullOrderDetails
     {
         public int ID { get; set; }
+        public string OrderTitle { get; set; }
         public string OrderDescription { get; set; }
         public DateTime StartingDate { get; set; }
         public string Commentary { get; set; }
