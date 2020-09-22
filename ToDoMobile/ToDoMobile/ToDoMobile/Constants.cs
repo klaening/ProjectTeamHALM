83209ace5363df29568c628d15fda51331e182e4
Huy Tran <Nicudes@gmail.com>
2020-09-22 11:06:42 +0200

    Fixing push

---
+++ ToDoMobile/ToDoMobile/ToDoMobile/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ToDoMobile
+{
+   public static class Constants
+    {
+
+        public const string ListenConnectionString = "Endpoint=sb://teamhalmtestnotification.servicebus.windows.net/;SharedAccessKeyName=DefaultListenSharedAccessSignature;SharedAccessKey=WUbMxbw6buzkr1DLf+WCQKB6wzOUY1eY4k+wZlVLLek=";
+        public const string NotificationHubName = "TeamHalmTestNotification";
+    }
+}
