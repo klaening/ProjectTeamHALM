76a9ac1704d58a1a8505857f91c808d41f55893f
Mikael <m.klaening@gmail.com>
2020-09-23 12:49:01 +0200

    Splash Screen, Login Screen and Scroll bar for ListView

---
+++ TeamHALM_Desktop/ToDo_Desktop/ToDo_Desktop/Views/LoginPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using ToDo_Desktop.Models;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ToDo_Desktop.Views
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class LoginPage : Page
+    {
+        public LoginPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void LoginBtn_Clicked(object sender, RoutedEventArgs e)
+        {
+            ActiveUser.Staff = new Staff
+            {
+                ID = 4,
+                StaffName = "Admin1",
+                DepartmentsID = 4
+            };
+            Frame.Navigate(typeof(AdminMainPage));
+        }
+    }
+}
